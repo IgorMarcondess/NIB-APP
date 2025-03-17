22a1e08484ebcebd429d3f2ab2141a7ce908bb7a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-17 19:39:42 -0300

    Initial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import "../global.css"
-import { Slot } from 'expo-router'
 const RootLayout = () => {
   return (
     <View>
       <SafeAreaView>
-      <Text className='text-5xl bg-red-600'>testo a</Text>
-      <StatusBar style='dark'/>
-      <Slot />
+        <Text className='text-5xl bg-red-600'>testo b</Text>
+        <StatusBar style='dark' />
+        <Slot />
       </SafeAreaView>
     </View>
   )
