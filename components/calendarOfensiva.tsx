dabf092c5e7262d2822ef753dae24e6c85565bfd
Marcondezz <igorpmarcondes@yahoo.com>
2025-03-19 03:16:33 -0300

    Criando as paginas

--- components/calendarOfensiva.tsx
+++ components/calendarOfensiva.tsx
@@ -1,79 +1,79 @@
+import { format, startOfMonth, endOfMonth } from 'date-fns';
 import { Check, ChevronDown, ChevronUp } from 'lucide-react-native';
 import React, { useState } from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
-type AvaliacaoMensalProps = {
+type AvaliacaoProps = {
     ofensiva?: number;
     titulo?: string;
     children?: JSX.Element;
+    habitos?: Array<{ nome: string; feito: boolean }>;
 };
 
-export const CalendarOfensiva = (Props: AvaliacaoMensalProps) => {
+export const CalendarOfensiva = (Props: AvaliacaoProps) => {
 
-    const { ofensiva = 5, titulo = "Nome", children, ...rest } = Props;
+    const { ofensiva = 5, titulo = "Nome", children, 
+        habitos = [{ nome: "Escovação", feito: true },{ nome: "Fio dental", feito: false },{ nome: "Bochecho", feito: true },] , ...rest } = Props;
+
     const [mostrarAvaliacoes, setMostrarAvaliacoes] = useState(false);
-    const [corMedalha, setCorMedalha] = useState("")
-    const [date, setDate] = React.useState(new Date());
+    const [selectedDate, setSelectedDate] = useState("");
 
-    const getMedalColor = (medalha: string) => {
-        switch (medalha) {
-            case "ouro":
-                setCorMedalha("");
-            case "prata":
-                setCorMedalha("prata");
-            case "bronze":
-                return "#cd7f32";
-            default:
-                return "transparent";
-        }
-    }
+    const hoje = new Date();
+    const primeiroDia = format((hoje), "yyyy-MM-dd");
+    const ultimoDia = format((hoje), "yyyy-MM-dd");
+
     return (
-        <SafeAreaView>
+        <SafeAreaView className='p-5'>
             <TouchableOpacity
                 onPress={() => setMostrarAvaliacoes(!mostrarAvaliacoes)}
-                className="flex-row justify-start items-center bg-gray-300 rounded-2xl p-3 h-20">
-                <Text className="text-blue-700 font-extrabold text-2xl">{titulo}</Text>
-                <View className="flex-row items-start">
+                className="flex-row justify-start items-center bg-gray-300 rounded-2xl p-4 h-20">
+                <Text className="text-blue-700 font-extrabold text-2xl ">{titulo}</Text>
+                <View className="flex-row mr-auto ml-2">
                     {ofensiva > 20 ? (<Image source={require('../assets/Ouro.png')} />) : ofensiva > 10 ? (
                         <Image source={require('../assets/Prata.png')} />) : (<Image source={require('../assets/Bronze.png')} />)}
-                    {mostrarAvaliacoes ? (<ChevronUp size={20} color="black" className='mr-80' />) : (<ChevronDown size={20} color="black" className='mr-80' />)}
                 </View>
+                {mostrarAvaliacoes ? (<ChevronUp size={20} color="black" className='' />) : (<ChevronDown size={20} color="black" className='' />)}
             </TouchableOpacity>
 
             {mostrarAvaliacoes && (
-                <View className="bg-white p-3  rounded-2xl">
-                    <Text className="text-blue-700 font-bold text-md mb-2">
-                        AVALIAÇÕES DIÁRIAS
-                    </Text>
+                <View className="flex bg-white p-3  rounded-2xl items-center">
+                    <Text className="text-blue-700 font-bold text-md mb-2"> AVALIAÇÕES DIÁRIAS</Text>
 
+                        {/**COLOQUE AQ O CALENDÁRIO */}
+                        <Calendar onDayPress={(day:any) => setSelectedDate(day.dateString)}
+                        markedDates={{
+                            [selectedDate]: { selected: true, selectedColor: "blue" },
+                        }}
+                        minDate={primeiroDia}
+                        maxDate={ultimoDia}
+                        disableArrowLeft={true}
+                        disableArrowRight={true}
+                        theme={{
+                            todayTextColor: "blue",
+                            arrowColor: "black",
+                            textMonthFontWeight: "bold",
+                            textDayFontWeight: "500",
+                            selectedDayBackgroundColor: "blue",
+                            selectedDayTextColor: "#fff",
+                        }}
+                    />
 
                     {/* Data Selecionada */}
-                    <Text className="text-center text-blue-700 font-bold text-lg mt-3">
-                        10 DE SETEMBRO
-                    </Text>
+                    {selectedDate ? (
+                        <Text className="text-center text-blue-700 font-bold text-lg mt-3">{selectedDate}</Text>
+                    ) : (<Text className="text-center text-blue-700 font-bold text-lg mt-3">Selecione uma data</Text>)}
 
                     {/* Ícones de avaliação do dia */}
-                    <View className="flex-row justify-center space-x-4 mt-2">
-                        <View className="flex-row items-center">
-                            <Text className="text-blue-700 font-semibold mr-1">
-                                Escovação
-                            </Text>
-                            <Check size={18} color="green" />
-                        </View>
-
-                        <View className="flex-row items-center">
-                            <Text className="text-blue-700 font-semibold mr-1">
-                                Fio dental
-                            </Text>
-                            <Check size={18} color="green" />
-                        </View>
-
-                        <View className="flex-row items-center">
-                            <Text className="text-blue-700 font-semibold mr-1">Bochecho</Text>
-                            <Check size={18} color="green" />
-                        </View>
+                    <View className="flex-row items-center gap-6">
+                    {habitos.map((habito) => (
+                            <View key={habito.nome} className="flex-row items-center ">
+                                <Text className="text-blue-700 font-semibold mr-0.5">{habito.nome}</Text>
+                                <Check size={18} color={habito.feito ? "green" : "gray"} />
+                            </View>
+                        ))}
                     </View>
                 </View>
             )}
@@ -83,5 +83,13 @@ export const CalendarOfensiva = (Props: AvaliacaoMensalProps) => {
 
     )
 
+}
+
+function endOfMonth(hoje: Date): any {
+    throw new Error('Function not implemented.');
+}
+
+function startOfMonth(hoje: Date): any {
+    throw new Error('Function not implemented.');
 }
 
