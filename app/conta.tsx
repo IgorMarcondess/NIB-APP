77205cb7a06ed4dd36fc0ab1f61d475e89b4563d
Marcondezz <igorpmarcondes@yahoo.com>
2025-05-11 17:31:14 -0300

    Refinando últimos detalhes

--- app/conta.tsx
+++ app/conta.tsx
@@ -2,7 +2,7 @@ import { Link, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
 import { useState } from "react";
-import { Image, Modal, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, Modal, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useUser } from "../components/userContext";
 import { db } from "../services/firebase";
@@ -32,21 +32,21 @@ export default function Conta() {
             alert(`${field} atualizado com sucesso!`);
 
             const dados = await getDoc(userRef);
-            if (!dados?.exists()){
-                return;
-            }
-            const infosUsuario = dados.data()
-            const userData =  { cpfUser: infosUsuario.cpf,
-                                nomeUser: infosUsuario.nome,
-                                sobrenomeUser: infosUsuario.sobrenome,
-                                telefoneUser: infosUsuario.telefone,
-                                dataNascimentoUser: "",
-                                planoUser: infosUsuario.plano,
-                                emailUser: infosUsuario.email,
-                                idUser: `${dados.id}`
-                            }
+            if (!dados?.exists()) return;
 
-            setUser(userData)
+            const infosUsuario = dados.data();
+            const userData = {
+                cpfUser: infosUsuario.cpf,
+                nomeUser: infosUsuario.nome,
+                sobrenomeUser: infosUsuario.sobrenome,
+                telefoneUser: infosUsuario.telefone,
+                dataNascimentoUser: "",
+                planoUser: infosUsuario.plano,
+                emailUser: infosUsuario.email,
+                idUser: `${dados.id}`
+            };
+
+            setUser(userData);
         } catch (error) {
             console.error(`Erro ao atualizar ${field}:`, error);
             alert(`Erro ao atualizar ${field}.`);
@@ -70,46 +70,48 @@ export default function Conta() {
     };
 
     return (
-        <SafeAreaView className="flex-1 bg-white items-center">
-            <View className="flex-1 items-center pt-2">
-                <Image source={require("../assets/ManImage2.png")} className="w-[270px] h-[265px] mb-5" />
+        <SafeAreaView className="flex-1 bg-white">
+            <ScrollView contentContainerStyle={{ alignItems: "center", paddingBottom: 24 }} showsVerticalScrollIndicator={false}>
+                <View className="items-center pt-2">
+                    <Image source={require("../assets/ManImage2.png")} className="w-[200px] h-[200px] mb-5" />
 
-                <Text className="text-[#003EA6] text-lg mb-1">E-mail</Text>
-                <View className="w-[350px] h-[60px] justify-center items-center border border-blue-500 rounded-2xl bg-white shadow mb-2">
-                    <Text className="text-[#003EA6] text-lg">{user?.emailUser}</Text>
-                </View>
-                <TouchableOpacity onPress={() => setModalEmailVisivel(true)} className="mb-4">
-                    <Text className="text-[#003EA6] text-sm">Mudar E-mail</Text>
-                </TouchableOpacity>
-
-                <Text className="text-[#003EA6] text-lg mb-1">CPF</Text>
-                <View className="w-[350px] h-[60px] justify-center items-center border border-blue-500 rounded-2xl bg-white shadow mb-2">
-                    <Text className="text-[#003EA6] text-lg">{user?.cpfUser}</Text>
-                </View>
-                <TouchableOpacity onPress={() => setModalCpfVisivel(true)} className="mb-4">
-                    <Text className="text-[#003EA6] text-sm">Mudar CPF</Text>
-                </TouchableOpacity>
-
-                <Text className="text-[#003EA6] text-lg mb-1">Telefone</Text>
-                <View className="w-[350px] h-[60px] justify-center items-center border border-blue-500 rounded-2xl bg-white shadow mb-2">
-                    <Text className="text-[#003EA6] text-lg">{user?.telefoneUser}</Text>
-                </View>
-                <TouchableOpacity onPress={() => setModalTelefoneVisivel(true)} className="mb-4">
-                    <Text className="text-[#003EA6] text-sm">Mudar Telefone</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => setFecharModal(true)} className="bg-red-600 py-2 px-6 rounded-full mb-4">
-                    <Text className="text-white text-base font-bold">Deletar Conta</Text>
-                </TouchableOpacity>
-
-                <Link push href="/initial" asChild>
-                    <TouchableOpacity className="bg-[#003EA6] py-6 px-16 rounded-full mt-2">
-                        <Text className="text-white text-base font-bold">Voltar</Text>
+                    <Text className="text-[#003EA6] text-lg mb-1">E-mail</Text>
+                    <View className="w-[350px] h-[60px] justify-center items-center border border-blue-500 rounded-2xl bg-white shadow mb-2">
+                        <Text className="text-[#003EA6] text-lg">{user?.emailUser}</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => setModalEmailVisivel(true)} className="mb-4">
+                        <Text className="text-[#003EA6] text-sm">Mudar E-mail</Text>
                     </TouchableOpacity>
-                </Link>
 
-                <StatusBar style="light" />
-            </View>
+                    <Text className="text-[#003EA6] text-lg mb-1">CPF</Text>
+                    <View className="w-[350px] h-[60px] justify-center items-center border border-blue-500 rounded-2xl bg-white shadow mb-2">
+                        <Text className="text-[#003EA6] text-lg">{user?.cpfUser}</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => setModalCpfVisivel(true)} className="mb-4">
+                        <Text className="text-[#003EA6] text-sm">Mudar CPF</Text>
+                    </TouchableOpacity>
+
+                    <Text className="text-[#003EA6] text-lg mb-1">Telefone</Text>
+                    <View className="w-[350px] h-[60px] justify-center items-center border border-blue-500 rounded-2xl bg-white shadow mb-2">
+                        <Text className="text-[#003EA6] text-lg">{user?.telefoneUser}</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => setModalTelefoneVisivel(true)} className="mb-4">
+                        <Text className="text-[#003EA6] text-sm">Mudar Telefone</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => setFecharModal(true)} className="bg-red-600 py-2 px-6 rounded-full mb-4">
+                        <Text className="text-white text-base font-bold">Deletar Conta</Text>
+                    </TouchableOpacity>
+
+                    <Link push href="/initial" asChild>
+                        <TouchableOpacity className="bg-[#003EA6] py-6 px-16 rounded-full mt-2">
+                            <Text className="text-white text-base font-bold">Voltar</Text>
+                        </TouchableOpacity>
+                    </Link>
+
+                    <StatusBar style="light" />
+                </View>
+            </ScrollView>
 
             <Modal visible={modalEmailVisivel} transparent animationType="slide">
                 <View className="flex-1 items-center justify-center bg-black/50">
@@ -121,7 +123,7 @@ export default function Conta() {
                             placeholder="Digite o novo e-mail"
                             className="w-full border border-blue-500 rounded-xl px-4 py-2 mb-4 text-[#003EA6]"
                         />
-                        <TouchableOpacity onPress={async () => { await atualizarDados("email", newEmail); setModalEmailVisivel(false);}}
+                        <TouchableOpacity onPress={async () => { await atualizarDados("email", newEmail); setModalEmailVisivel(false); }}
                             className="bg-[#003EA6] px-6 py-2 rounded-full mb-2"
                         >
                             <Text className="text-white font-bold">Confirmar</Text>
@@ -143,7 +145,7 @@ export default function Conta() {
                             placeholder="Digite o novo CPF"
                             className="w-full border border-blue-500 rounded-xl px-4 py-2 mb-4 text-[#003EA6]"
                         />
-                        <TouchableOpacity onPress={async () => { await atualizarDados("cpf", newCpf); setModalCpfVisivel(false)}}
+                        <TouchableOpacity onPress={async () => { await atualizarDados("cpf", newCpf); setModalCpfVisivel(false); }}
                             className="bg-[#003EA6] px-6 py-2 rounded-full mb-2"
                         >
                             <Text className="text-white font-bold">Confirmar</Text>
@@ -165,7 +167,7 @@ export default function Conta() {
                             placeholder="Digite o novo telefone"
                             className="w-full border border-blue-500 rounded-xl px-4 py-2 mb-4 text-[#003EA6]"
                         />
-                        <TouchableOpacity onPress={async () => { await atualizarDados("telefone", newTelefone); setModalTelefoneVisivel(false);}}
+                        <TouchableOpacity onPress={async () => { await atualizarDados("telefone", newTelefone); setModalTelefoneVisivel(false); }}
                             className="bg-[#003EA6] px-6 py-2 rounded-full mb-2"
                         >
                             <Text className="text-white font-bold">Confirmar</Text>
@@ -177,20 +179,17 @@ export default function Conta() {
                 </View>
             </Modal>
 
-
-            <Modal visible={fecharModal} transparent={true} animationType="fade">
+            <Modal visible={fecharModal} transparent animationType="fade">
                 <View className="flex-1 items-center justify-center bg-black/50">
                     <View className="bg-white p-6 rounded-2xl w-[85%] items-center">
                         <Text className="text-lg font-bold text-[#003EA6] mb-4">
                             Tem certeza que deseja excluir sua conta?
                         </Text>
                         <View className="flex-row space-x-4">
-                            <TouchableOpacity onPress={() => setFecharModal(false)} className="bg-gray-300 px-6 py-2 rounded-full"
-                            >
+                            <TouchableOpacity onPress={() => setFecharModal(false)} className="bg-gray-300 px-6 py-2 rounded-full">
                                 <Text className="text-black font-bold">Cancelar</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity onPress={deletarConta} className="bg-red-600 px-6 py-2 rounded-full"
-                            >
+                            <TouchableOpacity onPress={deletarConta} className="bg-red-600 px-6 py-2 rounded-full">
                                 <Text className="text-white font-bold">Sim</Text>
                             </TouchableOpacity>
                         </View>
