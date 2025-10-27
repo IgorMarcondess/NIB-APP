299467912b1cebe2521962cee413b25b26ed6635
IgorMarcondess <igorpmarcondes@yahoo.com>
2025-10-27 18:41:49 -0300

    Finalização

--- app/index.tsx
+++ app/index.tsx
@@ -7,13 +7,13 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 
 export default function Index() {
-
+const path = "/login"
   return (
     <SafeAreaView className="flex-1 bg-white items-center px-4 py-6">
 
       <Text className="text-center text-2xl text-blue-700 font-extrabold mb-16 mt-28 uppercase">Escolha uma opção</Text>
 
-      <TouchableOpacity onPress={() => router.push("../habitos/enviarImagem")} className="bg-[#F7FAFC] border border-blue-700 rounded-xl px-4 py-6 mb-6 w-80 h-44 items-center shadow-sm" >
+      <TouchableOpacity onPress={() => router.push(path)} className="bg-[#F7FAFC] border border-blue-700 rounded-xl px-4 py-6 mb-6 w-80 h-44 items-center shadow-sm" >
         <MaterialCommunityIcons name="account-check" size={56} color="#1D4ED8" style={{ marginBottom: 12 }}/>
 
         <Text className="text-blue-700 font-bold text-base">SOU CLIENTE</Text>
