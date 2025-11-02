b8f1c2723f847ecc7b7ada48ab943f079218561d
mabe <mariafogolin08@gmail.com>
2025-11-02 19:30:57 -0300

    Corrigindo a recuperação das notas

--- app/initial.tsx
+++ app/initial.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, Image, ScrollView, Platform, LogBox, FlatList, useWindowDimensions, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Platform,
+  LogBox,
+  FlatList,
+  useWindowDimensions,
+  Dimensions,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import BottomTabNavigator from "../components/navBottom";
 import { SafeAreaView } from "react-native-safe-area-context";
-import * as Notifications from 'expo-notifications';
+import * as Notifications from "expo-notifications";
 import { router } from "expo-router";
 import { useUser } from "../components/userContext";
 import CardNoticias from "../components/cardNoticias";
@@ -11,27 +22,25 @@ import { CalendarioCompleto } from "../components/calendarioCompleto";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../services/firebase";
 
-
-
 Notifications.setNotificationHandler({
-  handleNotification: async (): Promise<Notifications.NotificationBehavior> => ({
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-    shouldShowBanner: true,
-    shouldShowList: true,
-  }),
+  handleNotification:
+    async (): Promise<Notifications.NotificationBehavior> => ({
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+      shouldShowBanner: true,
+      shouldShowList: true,
+    }),
 });
 
 async function ensurePermissionsAndChannel() {
-  
   const { status } = await Notifications.getPermissionsAsync();
-  if (status !== 'granted') {
+  if (status !== "granted") {
     const req = await Notifications.requestPermissionsAsync();
-    if (req.status !== 'granted') throw new Error('Permissão negada');
+    if (req.status !== "granted") throw new Error("Permissão negada");
   }
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'Padrão',
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "Padrão",
       importance: Notifications.AndroidImportance.MAX,
     });
   }
@@ -42,10 +51,10 @@ export async function notifyNow(nome?: string) {
 
   await Notifications.scheduleNotificationAsync({
     content: {
-      title: `Oiii ${nome ?? 'você'}! Tudo bem?`,
-      body: 'Já realizou seu Check-in hoje? 👀',
+      title: `Oiii ${nome ?? "você"}! Tudo bem?`,
+      body: "Já realizou seu Check-in hoje? 👀",
     },
-    trigger: null, 
+    trigger: null,
   });
 }
 
@@ -59,28 +68,36 @@ export async function scheduleHourly(nome?: string) {
 
   await Notifications.scheduleNotificationAsync({
     content: {
-      title: `Oiii ${nome ?? 'você'}! tudo bem?`,
-      body: 'Já realizou seu Check-in hoje? 👀',
+      title: `Oiii ${nome ?? "você"}! tudo bem?`,
+      body: "Já realizou seu Check-in hoje? 👀",
     },
     trigger,
   });
 }
 
 const Dashboard = () => {
- const { user } = useUser();
+  const { user } = useUser();
   const [ofensiva, setOfensiva] = useState(0);
 
   useEffect(() => {
-
-      scheduleHourly(user?.nomeUser).catch(console.warn);
+    scheduleHourly(user?.nomeUser).catch(console.warn);
 
     const buscarDiasPreenchidos = async () => {
-      
       if (!user?.idUser) return;
 
       const meses = [
-        "janeiro", "fevereiro", "março", "abril", "maio", "junho",
-        "julho", "agosto", "setembro", "outubro", "novembro", "dezembro",
+        "janeiro",
+        "fevereiro",
+        "março",
+        "abril",
+        "maio",
+        "junho",
+        "julho",
+        "agosto",
+        "setembro",
+        "outubro",
+        "novembro",
+        "dezembro",
       ];
       const hoje = new Date();
       const mesAtual = meses[hoje.getMonth()];
@@ -91,7 +108,7 @@ const Dashboard = () => {
         const docsUsuario = await getDoc(docRef);
         if (docsUsuario.exists()) {
           const dadosUsuario = docsUsuario.data();
-          
+
           if (dadosUsuario[mesAtual]) {
             const diasDoMes = dadosUsuario[mesAtual];
             const totalDias = Object.entries(diasDoMes).length;
@@ -113,44 +130,79 @@ const Dashboard = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-white p-5">
-      <ScrollView contentContainerStyle={{ paddingBottom: 80 }} className="flex-1" showsVerticalScrollIndicator={false}>
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 80 }}
+        className="flex-1"
+        showsVerticalScrollIndicator={false}
+      >
         <View className="bg-blue-600 p-4 rounded-lg">
           <View className="flex-row items-center gap-3">
-            <Image source={require("../assets/manImage.png")} className="w-14 h-14" />
+            <Image
+              source={require("../assets/manImage.png")}
+              className="w-14 h-14"
+            />
             <View>
               <Text className="text-white text-base font-bold leading-tight">
-                Olá, {user?.nomeUser.toUpperCase()} 
+                Olá, {user?.nomeUser.toUpperCase()}
               </Text>
-              <Text className="text-white text-sm font-semibold">Bem-vindos ao Teeth Diary</Text>
+              <Text className="text-white text-sm font-semibold">
+                Bem-vindos ao Teeth Diary
+              </Text>
             </View>
           </View>
         </View>
-        <TouchableOpacity onPress={() => router.push("./avaliacao")} className="items-center">
-          <Image source={require("../assets/slide/slide_1.png")} style={{ width: 300, height: 300, resizeMode: "contain" }}/> 
+        <TouchableOpacity
+          onPress={() => router.push("./avaliacao")}
+          className="items-center"
+        >
+          <Image
+            source={require("../assets/slide/slide_1.png")}
+            style={{ width: 300, height: 300, resizeMode: "contain" }}
+          />
         </TouchableOpacity>
         <View className="my-4 justify-center items-center">
-          <Text className="text-primary font-bold text-lg">Notícias Recentes</Text>
-          <Text className="text-primary font-semibold text-sm">Acompanhe as novidades do mundo odontológico</Text>
+          <Text className="text-primary font-bold text-lg">
+            Notícias Recentes
+          </Text>
+          <Text className="text-primary font-semibold text-sm">
+            Acompanhe as novidades do mundo odontológico
+          </Text>
           <CardNoticias />
         </View>
 
         <View className="flex-row items-center justify-center space-x-4 px-6 mb-6">
           <View className="mr-5">
             {ofensiva > 20 ? (
-              <Image source={require("../assets/Ouro_initial.jpg")} className="w-20 h-24" />
+              <Image
+                source={require("../assets/Ouro_initial.jpg")}
+                className="w-20 h-24"
+              />
             ) : ofensiva > 10 ? (
-              <Image source={require("../assets/Prata_initial.jpg")} className="w-20 h-24 " />
+              <Image
+                source={require("../assets/Prata_initial.jpg")}
+                className="w-20 h-24 "
+              />
             ) : (
-              <Image source={require("../assets/Bronze_initial.jpg")} className="w-20 h-24" />
+              <Image
+                source={require("../assets/Bronze_initial.jpg")}
+                className="w-20 h-24"
+              />
             )}
           </View>
           <View className="border-l-2 border-gray-300 pl-4">
-            <Text className="text-blue-700 font-bold text-3xl">{ofensiva} dias</Text>
+            <Text className="text-blue-700 font-bold text-3xl">
+              {ofensiva} dias
+            </Text>
             <Text className="text-gray-600 text-lg">CONSECUTIVOS</Text>
           </View>
         </View>
-        <TouchableOpacity className="bg-gray-300 p-4 m-4 rounded-xl items-center"onPress={() => router.push("/habitos/avaliacao")}>
-          <Text className="text-blue-700 font-bold text-lg">ENVIE SEU HÁBITO DIÁRIO</Text>
+        <TouchableOpacity
+          className="bg-gray-300 p-4 m-4 rounded-xl items-center"
+          onPress={() => router.push("/habitos/avaliacao")}
+        >
+          <Text className="text-blue-700 font-bold text-lg">
+            ENVIE SEU HÁBITO DIÁRIO
+          </Text>
           <Text className="text-blue-500">Clique Aqui!</Text>
         </TouchableOpacity>
 
@@ -159,9 +211,26 @@ const Dashboard = () => {
 
       <BottomTabNavigator
         icons={[
-          { name: "Ofensiva", component: <Feather name="clock" size={24} color="white" />, route: "/ofensiva" },
-          { name: "Historico", component: <Feather name="clipboard" size={24} color="white" />, route: "/historico-medico" },
-          { name: "Troféus", component: <Feather name="award" size={24} color="white" />, route: "/rank/rankUser" },
+          {
+            name: "Historico",
+            component: <Feather name="clipboard" size={24} color="white" />,
+            route: "/historico-medico",
+          },
+          {
+            name: "Ofensiva",
+            component: <Feather name="clock" size={24} color="white" />,
+            route: "/ofensiva",
+          },
+          {
+            name: "Diário",
+            component: <Feather name="book-open" size={24} color="white" />,
+            route: "/habitos/avaliacao",
+          },
+          {
+            name: "Troféus",
+            component: <Feather name="award" size={24} color="white" />,
+            route: "/rank/rankUser",
+          },
         ]}
       />
     </SafeAreaView>
--- app/rank/rankUser.tsx
+++ app/rank/rankUser.tsx
@@ -2,177 +2,225 @@ import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { db } from "../../services/firebase";
 import { useUser } from "../../components/userContext";
-import { Image, Modal, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  Modal,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import axios from "axios";
 import { router } from "expo-router";
 import { API } from "../../src/constants";
 
-type AllpontuacoesType = { 
-  cpfUser: string; 
+type AllpontuacoesType = {
+  cpfUser: string;
   dataNascimentoUser: string;
-  emailUser: string; 
+  emailUser: string;
   endereco: unknown | null;
-  nomeUser: string; 
-  nota: number; 
-  planoUser: string;                
-  pontos: number;                   
-  sequenciaDias: number;            
-  sobrenomeUser: string;            
-  telefoneUser: string;        
-}
-
-export default function Usuario(){
-const [allpontuacoes, setAllpontuacoes] = useState<AllpontuacoesType[]>([]);
-const [pontuacaoUser, setPontuacaoUser] = useState();
-const [notaUser, setNotaUser] = useState(0)
-const [visible, setVisible] = useState(false); 
-const { user } = useUser()
-
-const imagensPerfil = [
-  require('../../assets/Woman 3.png'),
-  require('../../assets/Woman 3.png'),
-  require('../../assets/Woman 4.png'),
-]
-
-
-useEffect(() => {
-const fetchPontuacoesCpf = async () => {
-  try {
-    const response = await axios.get(`http://${API.BASE_URL}/usuario/cpf/${user?.cpfUser}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setNotaUser(response.data.nota)
-    setPontuacaoUser(response.data.pontos)
-  } catch (error) {
-    console.error("Erro ao buscar pontuação do usuário: ", error)
-  }
+  nomeUser: string;
+  nota: number;
+  planoUser: string;
+  pontos: number;
+  sequenciaDias: number;
+  sobrenomeUser: string;
+  telefoneUser: string;
 };
-fetchPontuacoesCpf
 
-const fetchPontuacoes = async () => {
+export default function Usuario() {
+  const [allpontuacoes, setAllpontuacoes] = useState<AllpontuacoesType[]>([]);
+  const [pontuacaoUser, setPontuacaoUser] = useState();
+  const [notaUser, setNotaUser] = useState(0);
+  const [visible, setVisible] = useState(false);
+  const { user } = useUser();
 
-  try {
-    const response = await axios.get<AllpontuacoesType[]>(`http://${API.BASE_URL}/usuario/todos`, {
-      headers: {
-        "Content-Type": "application/json",
+  const imagensPerfil = [
+    require("../../assets/Woman 3.png"),
+    require("../../assets/Woman 3.png"),
+    require("../../assets/Woman 4.png"),
+  ];
+
+  useEffect(() => {
+    const fetchPontuacoesCpf = async () => {
+      try {
+        const response = await axios.get(
+          `http://${API.BASE_URL}/usuario/cpf/${user?.cpfUser}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setNotaUser(response.data.nota);
+        setPontuacaoUser(response.data.pontos);
+      } catch (error) {
+        console.error("Erro ao buscar pontuação do usuário: ", error);
       }
-    });
+    };
 
-    setAllpontuacoes(response.data)
-  } catch (error) {
-    console.error("Erro ao buscar pontuações de todos os usuários:", error);
+    const fetchPontuacoes = async () => {
+      try {
+        const response = await axios.get<AllpontuacoesType[]>(
+          `http://${API.BASE_URL}/usuario/todos`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setAllpontuacoes(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar pontuações de todos os usuários:", error);
+      }
+    };
+
+    if (user?.cpfUser) {
+      fetchPontuacoesCpf(); // ✅ chamada correta
+      fetchPontuacoes(); // ✅ também chamada
+    }
+  }, [user?.cpfUser]);
+
+  function imagemAleatoria() {
+    const index = Math.floor(Math.random() * imagensPerfil.length);
+    return imagensPerfil[index];
   }
-};
-fetchPontuacoes()
 
-}, []);
+  return (
+    <SafeAreaView className="flex-1 bg-white px-4 justify-center items-center gap-4">
+      <View className="items-center mt-4">
+        <Text className="text-blue-700 font-bold text-2xl mt-6 mb-2">
+          SUA PONTUAÇÃO
+        </Text>
 
-function imagemAleatoria() {
-  const index = Math.floor(Math.random() * imagensPerfil.length);
-  return imagensPerfil[index];
-}
-
-
-return(
-
-  <SafeAreaView className="flex-1 bg-white px-4 justify-center items-center gap-4">
-    <View className="items-center mt-4">
-
-      <Text className="text-blue-700 font-bold text-2xl mt-6 mb-2">SUA PONTUAÇÃO</Text>
-
-      <View className="flex-row items-center bg-gray-100 rounded-xl px-4 py-2">
-        <Image source={require('../../assets/trophy.png')}className="w-5 h-5 mr-2"resizeMode="contain"/>
-        <Text className="text-blue-700 font-bold text-xl">{pontuacaoUser ?? 0}</Text>
+        <View className="flex-row items-center bg-gray-100 rounded-xl px-4 py-2">
+          <Image
+            source={require("../../assets/trophy.png")}
+            className="w-5 h-5 mr-2"
+            resizeMode="contain"
+          />
+          <Text className="text-blue-700 font-bold text-xl">
+            {pontuacaoUser ?? 0}
+          </Text>
+        </View>
       </View>
-    </View>
 
-    <View className="items-center justify-center mb-6">
-      <View className="flex flex-row justify-center items-center gap-2">
-      <Text className="text-blue-700 font-bold text-xl">NOTA</Text>
-        <TouchableOpacity onPress={() => setVisible(!visible)} className="w-6 h-6  bg-blue-700 rounded-full items-center justify-center">
-          <Text className="text-white font-bold">?</Text>
-        </TouchableOpacity>
-      </View>    
+      <View className="items-center justify-center mb-6">
+        <View className="flex flex-row justify-center items-center gap-2">
+          <Text className="text-blue-700 font-bold text-xl">NOTA</Text>
+          <TouchableOpacity
+            onPress={() => setVisible(!visible)}
+            className="w-6 h-6  bg-blue-700 rounded-full items-center justify-center"
+          >
+            <Text className="text-white font-bold">?</Text>
+          </TouchableOpacity>
+        </View>
         <Text className="font-bold text-4xl">{notaUser}</Text>
-    </View>
-
-    <View className=" bg-[#dee2e6] w-96 h-[38rem] rounded-xl p-5 gap-2 ">
-      <View className="items-center">
-      <Text className="text-blue-700 font-extrabold text-xl mb-2">RANKING</Text>
       </View>
-      <ScrollView className="flex-1" contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 24 }} showsVerticalScrollIndicator>
-      <View className="gap-4">
-      {allpontuacoes.sort((a, b) => b.pontos - a.pontos).map((user, index) => (
-        <View key={user.cpfUser} className="flex-row items-center justify-between bg-white rounded-2xl px-4 py-3 h-24">
-        
-            <View className="flex-row items-center space-x-3 gap-4">
-              <View className="w-10 h-10 bg-blue-700 rounded-full items-center justify-center">
-                <Text className="text-white font-extrabold text-xl">{index + 1}</Text>
-              </View>
-              <View className="rounded-full items-center justify-center">
-                <Image source={imagemAleatoria()} className="font-bold w-17 h-18"/>
-              </View>
-            </View>
 
-            <View className="items-center">
-            <Text className="text-lm font-bold">{user.nomeUser.toUpperCase()}</Text>
-            <Text className="text-blue-800 font-extrabold text-xl">{user.pontos} PTS</Text>
-            </View>
+      <View className=" bg-[#dee2e6] w-96 h-[38rem] rounded-xl p-5 gap-2 ">
+        <View className="items-center">
+          <Text className="text-blue-700 font-extrabold text-xl mb-2">
+            RANKING
+          </Text>
         </View>
-      ))}
+        <ScrollView
+          className="flex-1"
+          contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 24 }}
+          showsVerticalScrollIndicator
+        >
+          <View className="gap-4">
+            {allpontuacoes
+              .sort((a, b) => b.pontos - a.pontos)
+              .map((user, index) => (
+                <View
+                  key={user.cpfUser}
+                  className="flex-row items-center justify-between bg-white rounded-2xl px-4 py-3 h-24"
+                >
+                  <View className="flex-row items-center space-x-3 gap-4">
+                    <View className="w-10 h-10 bg-blue-700 rounded-full items-center justify-center">
+                      <Text className="text-white font-extrabold text-xl">
+                        {index + 1}
+                      </Text>
+                    </View>
+                    <View className="rounded-full items-center justify-center">
+                      <Image
+                        source={imagemAleatoria()}
+                        className="font-bold w-17 h-18"
+                      />
+                    </View>
+                  </View>
+
+                  <View className="items-center">
+                    <Text className="text-lm font-bold">
+                      {user.nomeUser.toUpperCase()}
+                    </Text>
+                    <Text className="text-blue-800 font-extrabold text-xl">
+                      {user.pontos} PTS
+                    </Text>
+                  </View>
+                </View>
+              ))}
+          </View>
+        </ScrollView>
       </View>
-      </ScrollView>
-    </View>
 
-    <TouchableOpacity onPress={() => router.push("/initial")} className="bg-primary py-3 px-8 rounded-full">
-      <Text className="text-white text-lg font-bold">Voltar</Text>
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => router.push("/initial")}
+        className="bg-primary py-3 px-8 rounded-full"
+      >
+        <Text className="text-white text-lg font-bold">Voltar</Text>
+      </TouchableOpacity>
 
-    <Modal animationType="slide" transparent={true} visible={visible}>
-      <View className="flex-1 bg-black/50 items-center justify-center px-4">
-        <View className="bg-white w-full rounded-3xl p-6 max-h-[90%]">
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <Text className="text-2xl font-extrabold text-center text-blue-800 mb-6">
-              COMO GANHAR MAIS PONTOS
-            </Text>
+      <Modal animationType="slide" transparent={true} visible={visible}>
+        <View className="flex-1 bg-black/50 items-center justify-center px-4">
+          <View className="bg-white w-full rounded-3xl p-6 max-h-[90%]">
+            <ScrollView showsVerticalScrollIndicator={false}>
+              <Text className="text-2xl font-extrabold text-center text-blue-800 mb-6">
+                COMO GANHAR MAIS PONTOS
+              </Text>
 
-            <View className="mb-5">
-              <Text className="text-lg font-bold text-gray-800 mb-1">
-                ESCOVE MAIS OS DENTES
-              </Text>
-              <Text className="text-gray-600">
-                Escove mais os dentes e registre mais no aplicativo.
-              </Text>
-            </View>
+              <View className="mb-5">
+                <Text className="text-lg font-bold text-gray-800 mb-1">
+                  ESCOVE MAIS OS DENTES
+                </Text>
+                <Text className="text-gray-600">
+                  Escove mais os dentes e registre mais no aplicativo.
+                </Text>
+              </View>
 
-            <View className="mb-5">
-              <Text className="text-lg font-bold text-gray-800 mb-1">
-                PASSE MAIS FIO DENTAL
-              </Text>
-              <Text className="text-gray-600">
-                Passe mais o fio dental para limpar de forma mais excelente e registre no aplicativo.
-              </Text>
-            </View>
+              <View className="mb-5">
+                <Text className="text-lg font-bold text-gray-800 mb-1">
+                  PASSE MAIS FIO DENTAL
+                </Text>
+                <Text className="text-gray-600">
+                  Passe mais o fio dental para limpar de forma mais excelente e
+                  registre no aplicativo.
+                </Text>
+              </View>
 
-            <View className="mb-8">
-              <Text className="text-lg font-bold text-gray-800 mb-1">
-                NÃO SE ESQUEÇA DE ENVIAR A SELFIE
-              </Text>
-              <Text className="text-gray-600">
-                Envie a selfie com boa iluminação e boa qualidade para que nossa IA contabilize seus pontos e notas..
-              </Text>
-            </View>
+              <View className="mb-8">
+                <Text className="text-lg font-bold text-gray-800 mb-1">
+                  NÃO SE ESQUEÇA DE ENVIAR A SELFIE
+                </Text>
+                <Text className="text-gray-600">
+                  Envie a selfie com boa iluminação e boa qualidade para que
+                  nossa IA contabilize seus pontos e notas..
+                </Text>
+              </View>
 
-            <TouchableOpacity onPress={() => setVisible(!visible)} className="bg-blue-700 py-3 rounded-2xl" >
-              <Text className="text-white text-center font-bold text-lg">
-                FECHAR
-              </Text>
-            </TouchableOpacity>
-          </ScrollView>
+              <TouchableOpacity
+                onPress={() => setVisible(!visible)}
+                className="bg-blue-700 py-3 rounded-2xl"
+              >
+                <Text className="text-white text-center font-bold text-lg">
+                  FECHAR
+                </Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
         </View>
-      </View>
-    </Modal>
-  </SafeAreaView>
-)}
+      </Modal>
+    </SafeAreaView>
+  );
+}
